bd68f0922b6eca69edbfaf5ab1396f6d8a1b699e
jeetkariya77777 <jeetkariya77777@gmail.com>
2023-01-02 10:24:03 +0530

    Commit

---
+++ 12.PrintPelindrome.py
@@ -0,0 +1,7 @@
+# WAP TO GET 2 NUMBER FROM THE USER AND PRINT PELINDROME NUMBER
+
+n1 = int(input("Enter 1st number: "))
+n2 = int(input("Enter 2nd number: "))
+list = [n1, n2]
+for i in list:
+    num = round(i / 10)
