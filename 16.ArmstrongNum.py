b47ddd7fce5ff856132abac240e8b80d594c435a
jeetkariya77777 <jeetkariya77777@gmail.com>
2023-01-06 10:25:05 +0530

    Commit

--- 16.ArmstrongNum.py
+++ 16.ArmstrongNum.py
@@ -3,5 +3,10 @@
 n = int(input("Enter a number: "))
 pw = int(input("Enter power of number: "))
 
-for i in str(n):
-    print(i)
+sum_pow = 0
+for i in s  tr(n):
+    sum_pow += int(i)**pw
+if n == sum_pow:
+    print(n, "is armstrong number")
+else:
+    print(n, "is not armstrong number")
---
+++ 17.3.PyramidOfOdd.py
@@ -0,0 +1,15 @@
+"""
+    1
+    3 5
+    7 9 11
+    13 15 17 19
+    ...n
+"""
+
+n = int(input("Enter a number of row : "))
+a = 0
+for i in range(1, n+1):
+    for j in range(1, i):
+        a += 1
+        if a % 2 != 0:
+            print(f'''{a}'''*i)
