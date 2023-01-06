4363bb1feec80921af44de90f5a47549c2be35a2
jeetkariya77777 <jeetkariya77777@gmail.com>
2023-01-06 09:40:47 +0530

    Commit

--- 12.PrintPelindrome.py
+++ 12.PrintPelindrome.py
@@ -4,22 +4,6 @@ n1 = int(input("Enter 1st number: "))
 n2 = int(input("Enter 2nd number: "))
 
 for i in range(n1, n2+1):
-    # convert the number to a string
     num_str = str(i)
-    # check if the number is a palindrome
     if num_str == num_str[::-1]:
         print(i)
-
-# for i in range(n1, n2+1):
-#     temp = i
-#     rev = 0
-#     n = 0
-#     while (n > 0):
-#         dig = i % 10
-#         rev = rev * 10 + dig
-#         n = i // 10
-#     print(temp == rev)
-    # if (temp == rev):
-    #     print(i, "is a palindrome")
-    # else:
-    #     print(i, "isn't a palindrome!")
---
+++ 16.ArmstrongNum.py
@@ -0,0 +1,7 @@
+# WAP TO CHECK NUMBER IS AMSTRONG OR NOT
+
+n = int(input("Enter a number: "))
+pw = int(input("Enter power of number: "))
+
+for i in str(n):
+    print(i)
